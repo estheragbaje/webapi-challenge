dada0d7a03b45c84a61da95099a7a2c7a6954b6a
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-01 10:19:57 +0100

    Created GET API for actionRouter

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -2,3 +2,19 @@ const express = require("express");
 const dbAction = require("./actionModel");
 
 const router = express.Router();
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  dbAction
+    .get(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "There was an error getting actions"
+      });
+    });
+});
+
+module.exports = router;
