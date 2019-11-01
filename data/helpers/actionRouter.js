9551a9e1ea119dd40a691aba1959c2bbe69b9035
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-01 10:41:15 +0100

    PUT API successfully created for actionRouter

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -17,4 +17,42 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const newAction = {
+    project_id: req.body.project_id,
+    description: req.body.description,
+    notes: req.body.notes
+  };
+  dbAction
+    .insert(newAction)
+    .then(data => {
+      res.status(201).json({
+        data,
+        message: "New Action successfully created"
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "There was an error adding action"
+      });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  dbAction
+    .update(id, changes)
+    .then(data => {
+      res.status(200).json({
+        message: "Action details successfully updated"
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        mesage: "There was an error updating action details"
+      });
+    });
+});
+
 module.exports = router;
